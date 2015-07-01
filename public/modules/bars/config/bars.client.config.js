95b01c2b909862bba6fe1939308d8c3350dce3d5
taapes <tapettis@gmail.com>
2015-07-01 16:11:12 -0700

    Google Maps
    added gmaps func to viewbar page.
    added commented todo items

--- public/modules/bars/config/bars.client.config.js
+++ public/modules/bars/config/bars.client.config.js
@@ -10,3 +10,11 @@ angular.module('bars').run(['Menus',
         */
 	}
 ]);
+
+angular.module('bars').config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyCY6exoka61eGfwNXTqR-abLnZb649j8TA',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+});
